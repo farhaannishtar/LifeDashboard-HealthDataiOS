c55a1092c71a2f4155964ee10f1368abbb10637b
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-06-01 14:43:38 +0530

    Initial Commit

---
+++ Life Dashboard/Life_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_DashboardApp.swift
+//  Life Dashboard
+//
+//  Created by Farhaan Nishtar on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Life_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
